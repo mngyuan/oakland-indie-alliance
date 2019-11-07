d2327043ddb455a5bbff8758ee02573b0927e36e
Kevin Lee <kevinmlee.x@gmail.com>
2019-11-06 16:32:48 -0800

    add rotating images to landing

--- src/App.js
+++ src/App.js
@@ -1,7 +1,41 @@
 // @flow
 //
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import Map from './Map';
+import {TransitionGroup, CSSTransition} from 'react-transition-group';
+
+const importAll = r => r.keys().map(r);
+const PHOTOS = importAll(
+  require.context('./static/photos', false, /\.(png|jpe?g|svg)$/i),
+);
+
+const RotatingBackgroundImage = ({imgs, className}) => {
+  const [curImage, setCurImage] = useState(0);
+  useEffect(() => {
+    const interval = setInterval(
+      () => setCurImage((curImage + 1) % imgs.length),
+      5000,
+    );
+    return function cleanup() {
+      clearInterval(interval);
+    };
+  });
+  const imgElems = imgs.map((src, i) => (
+    <img className="img-cover" src={src} key={i} />
+  ));
+
+  return (
+    <TransitionGroup className={`${className} full-cover-bg`}>
+      <CSSTransition
+        key={curImage}
+        timeout={{enter: 300, exit: 300}}
+        classNames="fade"
+      >
+        <img className="img-cover" src={imgs[curImage]} />
+      </CSSTransition>
+    </TransitionGroup>
+  );
+};
 
 class App extends React.PureComponent {
   componentDidMount() {
@@ -19,32 +53,36 @@ class App extends React.PureComponent {
       <>
         <section className="col full keep-it">
           <div className="row row-12">
-            <div className="col colc-6">
-              <span>
-                <span className="keep-it-text">Keep it</span>
-                <br />
-                <span className="keep-it-text" id="keep-it-oakland">
-                  Oakland
+            <div className="col colc-6 j-center a-center">
+              <div className="col padding-24">
+                <span>
+                  <span className="keep-it-text">Keep it</span>
+                  <br />
+                  <span className="keep-it-text" id="keep-it-oakland">
+                    Oakland
+                  </span>
                 </span>
-              </span>
-              <br />
-              <p>
-                The Keep It Oakland movement to inspire residents, visitors, and
-                anyone who enjoys our beloved Town to patronize and preserve
-                what makes Oakland so special: the flavor, color, and creativity
-                of its independent businesses.
-              </p>
-              <br />
-              <p>
-                When you shop, dine and experience local businesses, rock the
-                locally designed and produced Keep It Oakland merchandise or
-                share your love for the movement on social media, you are making
-                a powerful statement and demonstrating your commitment to the
-                independent businesses that define Oakland’s culture, community,
-                and local economy.
-              </p>
+                <br />
+                <p>
+                  The Keep It Oakland movement to inspire residents, visitors,
+                  and anyone who enjoys our beloved Town to patronize and
+                  preserve what makes Oakland so special: the flavor, color, and
+                  creativity of its independent businesses.
+                </p>
+                <br />
+                <p>
+                  When you shop, dine and experience local businesses, rock the
+                  locally designed and produced Keep It Oakland merchandise or
+                  share your love for the movement on social media, you are
+                  making a powerful statement and demonstrating your commitment
+                  to the independent businesses that define Oakland’s culture,
+                  community, and local economy.
+                </p>
+              </div>
             </div>
-            <div className="col colc-6">test</div>
+            <RotatingBackgroundImage className="col colc-6" imgs={PHOTOS}>
+              test
+            </RotatingBackgroundImage>
           </div>
         </section>
         <Map />
--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {SHOPIFY_TOKEN} from './KEYS';
 
 import App from './App';
 import './index.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
-window.shopifyToken = 'tmp';
+window.shopifyToken = SHOPIFY_TOKEN;
